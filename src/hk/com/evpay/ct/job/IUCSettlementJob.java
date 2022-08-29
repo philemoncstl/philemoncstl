f7e905a900084a02c3d81c4d2bdde249831756a7
philemon.fong <philemon.fong@cstl.com.hk>
2022-08-29 15:33:54 +0800

    update IUCSettlement if fail retry 10 time by 30 mins

--- src/hk/com/evpay/ct/job/IUCSettlementJob.java
+++ src/hk/com/evpay/ct/job/IUCSettlementJob.java
@@ -1,6 +1,5 @@
 package hk.com.evpay.ct.job;
 
-import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
@@ -10,11 +9,8 @@ import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
-import hk.com.cstl.evcs.ct.IucEventLogDto;
 import hk.com.evpay.ct.CtrlPanel;
-import hk.com.evpay.ct.util.CtUtil;
 import hk.com.evpay.ct.util.iUC285Util;
-import hk.com.evpay.ct.ws.CtWebSocketClient;
 
 public class IUCSettlementJob  implements Job{
 	private static final Logger logger = Logger.getLogger(IUCSettlementJob.class);
@@ -30,32 +26,58 @@ public class IUCSettlementJob  implements Job{
 				e.printStackTrace();
 			}
 		}
+		int retryCount = 0;
+		while(retryCount < 10) {
+			if(startIUCSettlement()) {
+				break;
+			}
+			retryCount ++;
+			try {
+				TimeUnit.MINUTES.sleep(30);
+			} catch (InterruptedException e) {
+				logger.error(e.getMessage());
+				e.printStackTrace();
+			}
+		}
+
+	}
+	
+	private boolean startIUCSettlement(){
 		logger.info("Ct in home page start doSettlement...");
-		CtrlPanel.goToErrorPage("cpStatusUnavailable");
-		JSONObject response = null;
-		response = iUC285Util.doSettlement("EDC");
-		boolean hideContactlessButton = false;
-		if (response != null && response.has("BATCH")) {
-			JSONArray c = response.getJSONArray("BATCH");
-			for(int i = 0 ; i < c.length(); i++) {
-				 JSONObject obj = c.getJSONObject(i);
-				 if("CommError".equals(obj.getString("STATUS")) || "doTranBad".equals(obj.getString("STATUS"))) {
-					CtrlPanel.hideContactlessButton();
-					break;
-				 }
-			}
-			if(hideContactlessButton) {
-				logger.info("hideContactlessButton");
-				hideContactlessButton = true;
+		boolean isApproved = true;
+		try {
+			CtrlPanel.goToErrorPage("cpStatusUnavailable");
+			JSONObject response = null;
+			response = iUC285Util.doSettlement("EDC");
+			boolean hideContactlessButton = false;
+			if (response != null && response.has("BATCH")) {
+				JSONArray c = response.getJSONArray("BATCH");
+				for(int i = 0 ; i < c.length(); i++) {
+					 JSONObject obj = c.getJSONObject(i);
+					 if(!"APPROVED".equals(obj.getString("STATUS"))) {
+						 isApproved = false;
+						break;
+					 }
+				}
+				CtrlPanel.hideContactlessButton();
+				if(hideContactlessButton) {
+					logger.info("hideContactlessButton");
+					hideContactlessButton = true;
+				} else {
+					logger.info("showContactlessButton");
+					CtrlPanel.showContactlessButton();
+				}
 			} else {
-				logger.info("showContactlessButton");
-				CtrlPanel.showContactlessButton();
+				isApproved = false;
+				logger.info("response == null || response do not has BATCH hideContactlessButton");
+				CtrlPanel.hideContactlessButton();
 			}
-		} else {
-			logger.info("hideContactlessButton");
-			CtrlPanel.hideContactlessButton();
+		}  catch (Exception e) {
+			logger.error("Failed to startIUCSettlement:" + e.getMessage(), e);
+			isApproved = false;
 		}
 		logger.info("Completed IUCSettlementJob");
 		CtrlPanel.goToHomePage();
+		return isApproved;
 	}
 }
